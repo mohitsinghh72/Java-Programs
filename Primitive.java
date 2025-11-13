2c11b58d6415a9d7a409debe3c806188f9eba1e7
Mohit Singh <mohitsinghh72@gmail.com>
2025-11-13 15:03:22 +0530

    Primitive Datatypes in Java

---
+++ Primitive.java
@@ -0,0 +1,15 @@
+public class Primitive {
+    public static void main(String[] args) {
+        // There are 8 primitive datatypes in java
+
+        int a = 12;
+        char b = 'z';
+        float c = 454.23f;
+        double LargeDecimalNumber = 324352232.322;
+        long LargeIntegerNumber = 123132134L;
+        short s = 1000;
+        byte d = 10;  // small Integers
+        boolean j = true;
+
+    }
+}
