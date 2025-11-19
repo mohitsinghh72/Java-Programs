0c619dfab581333de0dc1da05154bec08c42d0bd
Mohit Singh <mohitsinghh72@gmail.com>
2025-11-20 01:08:36 +0530

    Factorial of number using Recursion

---
+++ FactorialRecursion.java
@@ -0,0 +1,12 @@
+public class FactorialRecursion {
+    public static void main(String[] args) {
+        int ans = fact(5);
+        System.out.println(ans);
+    }
+    static int fact(int n){
+        if(n == 1){
+            return n;
+        }
+        return n* fact(n-1);
+    }
+}
