687090ffcf3893db729ad4ddb85a79770fc75cf3
Mohit Singh <mohitsinghh72@gmail.com>
2025-11-21 23:44:28 +0530

    Skip the word form String

---
+++ SkipWordRecursion.java
@@ -0,0 +1,28 @@
+public class SkipWordRecursion {
+    public static void main(String[] args) {
+        String ans =skipAppNotApple("appasdaappleasdapp");
+        System.out.println(ans);
+        String ans2 = skipApple("applesadasf");
+        System.out.println(ans2);
+    }
+    static String skipAppNotApple(String str){
+        if(str.isEmpty()){
+            return " ";
+        }
+        if(str.startsWith("app") && !str.startsWith("apple")){
+            return skipAppNotApple(str.substring(3));
+        }else{
+            return str.charAt(0)+skipAppNotApple(str.substring(1));
+        }
+    }
+    static String skipApple(String str){
+        if(str.isEmpty()){
+            return "";
+        }
+        if(str.startsWith("apple")){
+            return skipApple(str.substring(5));
+        }else{
+            return str.charAt(0)+str.substring(1);
+        }
+    }
+}
